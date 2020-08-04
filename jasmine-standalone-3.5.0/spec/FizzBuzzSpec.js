1960b36113bced30c0736e9ae0b73b96e1c4acc7
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-08-04 12:48:27 +0100

    DRY's instanciation of fizzbuzz in each test to before block, adds test for '5'

--- jasmine-standalone-3.5.0/spec/FizzBuzzSpec.js
+++ jasmine-standalone-3.5.0/spec/FizzBuzzSpec.js
@@ -3,18 +3,27 @@ describe('FizzBuzz', function() {
   var fizzBuzz;
   // sets the scope of the variable
 
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+  // to DRY tests up, no need to instantiate fizzbuzz each time.
+
   describe('knows when a number is', function(){
   //use this instead of context to write meaningful tests
     it('divisible by 3', function(){
-      fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is not', function(){
     it('divisible by 3', function(){
-      fizzBizz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(5)).toBe(false);
+    });
+  });
+
+  describe('knows number is', function(){
+    it('divisible by 5', function(){
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
     });
   });
 });
